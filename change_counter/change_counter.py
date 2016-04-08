eef0a8db29d36107de621ad80891e66c9f3a91e0
Philip Tyler <philip.b.tyler@gmail.com>
2016-04-07 22:12:28 -0700

    adding change counter project file

---
+++ change_counter/change_counter.py
@@ -0,0 +1,27 @@
+"""
+Change counter!
+
+1) Create a function, calculate_change(total_cost, total_paid) which returns float of change to give
+2) Create function,  convert_tender(change_owed), which returns human-readable string of dollars, quarters
+	dimes, nickles, and pennies owed
+3) Create a function, main_loop(), that when run in the console will continually ask the user for a
+	total cost and total paid then print the human-readable string of change
+"""
+
+# Example output: 2.66 (make sure its only 2 decimal places! your parameters may not be)
+def calculate_change(total_cost, total_paid):
+
+
+# Example output: "$2.33 owed: 2 dollars, 1 quater, 0 dimes, 1 nickel, and 3 pennies"
+def convert_tender(change_owed):
+
+
+# Use the input function to get input from user:
+# total = input("Please Enter Total:")
+def main_loop():
+
+
+
+# HERE'S SOME TESTS
+calculate_change(15.50, 20) == 4.50
+convert_tender(2.33) == "$2.33 owed: 2 dollars, 1 quater, 0 dimes, 1 nickel, and 3 pennies"
