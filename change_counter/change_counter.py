d6e974e7b232944d856afd11ee0622009e4af96c
Yaniv Goldobin <ygoldibn@gmail.com>
2016-04-11 00:50:10 -0700

    made first steps in change_counter

--- change_counter/change_counter.py
+++ change_counter/change_counter.py
@@ -8,20 +8,39 @@ Change counter!
 	total cost and total paid then print the human-readable string of change
 """
 
+# Input Section
+total_cost = input("What was the price?:")
+total_paid = input("What did you pay?:") 
+
 # Example output: 2.66 (make sure its only 2 decimal places! your parameters may not be)
+
 def calculate_change(total_cost, total_paid):
+	diff = (total_paid) - (total_cost)
+	return diff
+
+
+difference = calculate_change(total_cost, total_paid)
+if difference < 0:
+	print "you are", difference, "under"
+elif difference > 0:
+	print "you get", difference, "change"
+
+
 
 
 # Example output: "$2.33 owed: 2 dollars, 1 quater, 0 dimes, 1 nickel, and 3 pennies"
-def convert_tender(change_owed):
+# def convert_tender(change_owed):
 
 
 # Use the input function to get input from user:
 # total = input("Please Enter Total:")
-def main_loop():
+
+# print total
+
+#def main_loop():
 
 
 
 # HERE'S SOME TESTS
-calculate_change(15.50, 20) == 4.50
-convert_tender(2.33) == "$2.33 owed: 2 dollars, 1 quater, 0 dimes, 1 nickel, and 3 pennies"
+# calculate_change(15.50, 20) == 4.50
+# convert_tender(2.33) == "$2.33 owed: 2 dollars, 1 quater, 0 dimes, 1 nickel, and 3 pennies"
